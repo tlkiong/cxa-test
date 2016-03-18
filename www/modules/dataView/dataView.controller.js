d7a6fe44d4c124c4e022715728817501a7ef1e52
Kiong Kiong <kiong90@gmail.com>
2016-03-19 02:41:22 +0800

    Done design for dataView page

--- www/modules/dataView/dataView.controller.js
+++ www/modules/dataView/dataView.controller.js
@@ -8,23 +8,30 @@
 
     function dataViewController(dataViewService, commonService) {
         var vm = this;
+        vm.sortBy = sortBy;
 
         /* ======================================== Var ==================================================== */
-        vm.misc = {};
+        vm.misc = {
+            searchTxt: '',
+            sortBy: ''
+        };
+        vm.data = [];
 
         /* ======================================== Services =============================================== */
         var svc = dataViewService;
         var cmnSvc = commonService;
 
         /* ======================================== Public Methods ========================================= */
+        function sortBy(type, isAscending) {
+            
+        }
 
         /* ======================================== Private Methods ======================================== */
         function init() {
             svc.getData(true).then(function(rs){
-                alert('success!');
-                console.log(rs);
+                angular.copy(rs.data, vm.data);
             }, function(err){
-                alert(err);
+                alert('Error!');
                 console.log(err);
             });
         }
