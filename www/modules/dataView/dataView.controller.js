7dbc6c0416fe4dc45b597147eb81da60adef2c6a
Kiong Kiong <kiong90@gmail.com>
2016-03-19 02:02:17 +0800

    Able to get sample data from URL

--- www/modules/dataView/dataView.controller.js
+++ www/modules/dataView/dataView.controller.js
@@ -4,22 +4,29 @@
     angular.module('DataView')
         .controller('dataViewController', dataViewController);
 
-    dataViewController.$inject = ['commonService'];
+    dataViewController.$inject = ['dataViewService', 'commonService'];
 
-    function dataViewController(commonService) {
+    function dataViewController(dataViewService, commonService) {
         var vm = this;
 
         /* ======================================== Var ==================================================== */
         vm.misc = {};
 
         /* ======================================== Services =============================================== */
+        var svc = dataViewService;
         var cmnSvc = commonService;
 
         /* ======================================== Public Methods ========================================= */
 
         /* ======================================== Private Methods ======================================== */
         function init() {
-            
+            svc.getData(true).then(function(rs){
+                alert('success!');
+                console.log(rs);
+            }, function(err){
+                alert(err);
+                console.log(err);
+            });
         }
 
         init();
--- www/modules/dataView/dataView.service.js
+++ www/modules/dataView/dataView.service.js
@@ -4,15 +4,31 @@
 	angular.module('DataView')
 		.service('dataViewService', dataViewService);
 
-		dataViewService.$inject = [];
-		function dataViewService() {
+		dataViewService.$inject = ['httpRequestService', 'commonService'];
+		function dataViewService(httpRequestService, commonService) {
 			var service = this;
+			service.getData = getData;
 
 			/* ======================================== Var ==================================================== */
 
 	        /* ======================================== Services =============================================== */
+	        var cmnSvc = commonService;
+	        var httpSvc = httpRequestService;
 
 	        /* ======================================== Public Methods ========================================= */
+	        function getData(isSample) {
+	        	var deferred = cmnSvc.$q.defer();
+
+	        	if(isSample == true) {
+	        		httpSvc.http('get_sample_data').then(function(rs){
+	        			deferred.resolve(rs);
+	        		}, function(err) {
+	        			deferred.reject(err);
+	        		})
+	        	}
+
+	        	return deferred.promise;
+	        }
 
 	        /* ======================================== Private Methods ======================================== */
 		}
