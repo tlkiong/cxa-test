bada001d9b1773e670886942a58bd078970cd331
Kiong Kiong <kiong90@gmail.com>
2016-03-27 14:41:31 +0800

    Done loading spinner & reload for inital load of data

--- www/modules/dataView/dataView.controller.js
+++ www/modules/dataView/dataView.controller.js
@@ -8,6 +8,7 @@
 
     function dataViewController($scope, $mdMedia, $mdDialog, dataViewService, commonService) {
         var vm = this;
+        vm.loadData = loadData;
         vm.sortBy = sortBy;
         vm.resetTable = resetTable;
         vm.groupBy = groupBy;
@@ -18,9 +19,12 @@
             searchTxt: '',
             orderVal: '',
             groupByCol: '',
-            isReversed: false
+            isReversed: false,
+            errorLoadingData: false
         };
         vm.data = [];
+
+        var spinner;
 
         /* ======================================== Services =============================================== */
         var svc = dataViewService;
@@ -59,7 +63,7 @@
                     }
                 }
 
-                console.log('list: ',dataObj);
+                console.log('list: ', dataObj);
 
                 var fullscreen = ($mdMedia('sm') || $mdMedia('xs')) && ($mdMedia('xs') || $mdMedia('sm'));
                 mdD.show({
@@ -124,7 +128,46 @@
             }
         }
 
+        function loadData() {
+            svc.getData(true).then(function(rs) {
+                angular.copy(rs.data, vm.data);
+                spinner.stop();
+            }, function(err) {
+                spinner.stop();
+                vm.misc.errorLoadingData = true;
+                alert('Error!');
+                console.log(err);
+            });
+        }
+
         /* ======================================== Private Methods ======================================== */
+        function spinner() {
+            var opts = {
+                lines: 13, // The number of lines to draw
+                length: 28, // The length of each line
+                width: 14, // The line thickness
+                radius: 42, // The radius of the inner circle
+                scale: 1, // Scales overall size of the spinner
+                corners: 1, // Corner roundness (0..1)
+                color: '#000', // #rgb or #rrggbb or array of colors
+                opacity: 0.25, // Opacity of the lines
+                rotate: 0, // The rotation offset
+                direction: 1, // 1: clockwise, -1: counterclockwise
+                speed: 1, // Rounds per second
+                trail: 60, // Afterglow percentage
+                fps: 20, // Frames per second when using setTimeout() as a fallback for CSS
+                zIndex: 2e9, // The z-index (defaults to 2000000000)
+                className: 'spinner', // The CSS class to assign to the spinner
+                top: '50%', // Top position relative to parent
+                left: '50%', // Left position relative to parent
+                shadow: false, // Whether to render a shadow
+                hwaccel: false, // Whether to use hardware acceleration
+                position: 'absolute'
+            }
+            var target = document.getElementById('loadingContainer')
+            spinner = new Spinner(opts).spin(target);
+        }
+
         function dialog($scope, $mdDialog, items) {
             /* =========== Var =========== */
             $scope.items = items;
@@ -143,12 +186,8 @@
         }
 
         function init() {
-            svc.getData(true).then(function(rs) {
-                angular.copy(rs.data, vm.data);
-            }, function(err) {
-                alert('Error!');
-                console.log(err);
-            });
+            spinner();
+            loadData();
         }
 
         init();
