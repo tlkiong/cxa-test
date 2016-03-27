d80461ed16b7b13a57f4992d21bf544744ed57a6
Kiong Kiong <kiong90@gmail.com>
2016-03-27 14:18:38 +0800

    Done design for pop up for full details of user

--- www/modules/dataView/dataView.controller.js
+++ www/modules/dataView/dataView.controller.js
@@ -1,16 +1,17 @@
 (function() {
     'use strict';
-    
+
     angular.module('DataView')
         .controller('dataViewController', dataViewController);
 
-    dataViewController.$inject = ['dataViewService', 'commonService'];
+    dataViewController.$inject = ['$scope', '$mdMedia', '$mdDialog', 'dataViewService', 'commonService'];
 
-    function dataViewController(dataViewService, commonService) {
+    function dataViewController($scope, $mdMedia, $mdDialog, dataViewService, commonService) {
         var vm = this;
         vm.sortBy = sortBy;
         vm.resetTable = resetTable;
         vm.groupBy = groupBy;
+        vm.showDetails = showDetails;
 
         /* ======================================== Var ==================================================== */
         vm.misc = {
@@ -24,10 +25,75 @@
         /* ======================================== Services =============================================== */
         var svc = dataViewService;
         var cmnSvc = commonService;
+        var mdD = $mdDialog;
+        var mdMedia = $mdMedia;
 
         /* ======================================== Public Methods ========================================= */
+        function showDetails(index) {
+            var dataObj = {};
+            angular.copy(vm.data[index], dataObj);
+
+            delete dataObj.$$hashKey; // Remove added key by Angular during ng-repeat
+
+            if (cmnSvc.isObjPresent(dataObj)) {
+                for (var key in dataObj) {
+                    if (dataObj.hasOwnProperty(key)) {
+                        if (key === 'contactnumber') {
+                            var tmpList = [];
+                            dataObj[key].forEach(function(e) {
+                                tmpList.push('(' + e.phoneType + ') ' + e.phone);
+                            });
+                            dataObj[key] = tmpList;
+                        } else if (key === 'friends') {
+                            var tmpList = [];
+                            dataObj[key].forEach(function(e) {
+                                tmpList.push(e.name);
+                            });
+
+                            dataObj[key] = tmpList;
+                        } else if (key === 'name') {
+                            dataObj[key] = dataObj[key].first + ' ' + dataObj[key].last;
+                        } else if (key === 'tags') {
+                            dataObj[key] = dataObj[key];
+                        }
+                    }
+                }
+
+                console.log('list: ',dataObj);
+
+                var fullscreen = ($mdMedia('sm') || $mdMedia('xs')) && ($mdMedia('xs') || $mdMedia('sm'));
+                mdD.show({
+                        controller: dialog,
+                        templateUrl: './modules/dataView/dataDetails.html',
+                        parent: angular.element(document.body),
+                        // targetEvent: ev,
+                        clickOutsideToClose: true,
+                        locals: {
+                            items: dataObj
+                        },
+                        fullscreen: fullscreen
+                    })
+                    .then(function(rs) {
+                        console.log('Yeah! ', rs);
+                    }, function(er) {
+                        console.log('hmm')
+                    });
+            } else {
+                $mdDialog.alert({
+                    title: 'Error',
+                    textContent: 'No details for the chosen data',
+                    ok: 'Close'
+                });
+                $mdDialog
+                    .show(alert)
+                    .finally(function() {
+                        alert = undefined;
+                    });
+            }
+        }
+
         function groupBy() {
-            
+
         }
 
         function resetTable() {
@@ -38,7 +104,7 @@
         }
 
         function sortBy(type, isReverse) {
-            if(cmnSvc.isObjPresent(type) && cmnSvc.isObjPresent(isReverse)) {
+            if (cmnSvc.isObjPresent(type) && cmnSvc.isObjPresent(isReverse)) {
                 if (type === 'firstName') {
                     vm.misc.orderVal = 'name.first';
                 } else if (type === 'lastName') {
@@ -59,10 +125,27 @@
         }
 
         /* ======================================== Private Methods ======================================== */
+        function dialog($scope, $mdDialog, items) {
+            /* =========== Var =========== */
+            $scope.items = items;
+
+            /* ===== Public Methods ====== */
+            $scope.hide = hide;
+            $scope.cancel = cancel;
+
+            function hide(val) {
+                $mdDialog.hide(val);
+            };
+
+            function cancel() {
+                $mdDialog.cancel();
+            };
+        }
+
         function init() {
-            svc.getData(true).then(function(rs){
+            svc.getData(true).then(function(rs) {
                 angular.copy(rs.data, vm.data);
-            }, function(err){
+            }, function(err) {
                 alert('Error!');
                 console.log(err);
             });
